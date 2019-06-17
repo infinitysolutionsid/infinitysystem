5d0fa0530578bf8d371a8d2767181a1633899995
Bintang Jeremia Tobing <hello@bintangtobing.com>
2019-06-17 11:43:39 +0700

    upload

---
+++ resources/views/user.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.layout')
+@section('content')
+              <div class="col-md-12 top-20 padding-0">
+                <div class="col-md-12">
+                  <div class="panel">
+                    <div class="panel-heading"><h3>User Data</h3></div>
+                    <div class="panel-body">
+                      <div class="responsive-table">
+                      <table id="datatables-example" class="table table-striped table-bordered" width="100%" cellspacing="0">
+                      <thead>
+                        <tr>
+                          <th>Username</th>
+                          <th>Full Name</th>
+                        <th>Type</th>
+                        <th>Password</th>
+                        <th>Status</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                      </tbody>
+                        </table>
+                      </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+@endsection
